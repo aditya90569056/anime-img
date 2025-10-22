0c9f44e288224d0e0fff41526155f123c538a658
aditya nath <126580063+aditya90569056@users.noreply.github.com>
2025-10-22 20:01:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// import { saveAs } from 'file-saver';
+bg = document.getElementById("main");
+async  function fetch1(){
+data = await  fetch(`https://api.waifu.pics/nsfw/waifu`);
+data = await data.json();
+bg.style.backgroundImage = `url(${data.url})`
+img = data.url;
+console.log(data.url)
+
+}
+function get(){
+    saveAs(img, "anime.jpg");
+   }
+
+
+// fetch1()
